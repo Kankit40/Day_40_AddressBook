1671299e863f3988be8fcd8b945fb0a52f3c555c
Kankit40 <Kankit40@gmail.com>
2023-01-07 19:07:46 +0530

    UC 7: Avoid Duplicate

--- Address_Book.js
+++ Address_Book.js
@@ -111,12 +111,14 @@ class Contacts {
             + " Email = " + this.email;
     }
 }
-
+// UC1:Adding the contact in the address Book
+// let contact1 = new Contacts("Sahil", "Khan", "Ambazari", "Nagpur", "MHGJ", "447141", "987962080", "sahil@gmail.com");
+//console.log(contact1.toString());
 
 //UC3: Creating array of contacts
-let arr=new Array(new Contacts("Amit", "Kumar", "Padam", "Ballabgarh", "Haryana", "121004", "0898989899", "Kankit@gm.com"), 
-new Contacts("Ankush", "Kumar", "Sector", "Palwal", "Haryana", "121007", "0898969899", "Kankit@gm2.com"),
-new Contacts("Sumit", "Sharma", "Sector", "OldFaridabad", "Haryana", "121504", "0798989899", "Kankit@gm3.com"));
+let arr = new Array(new Contacts("Sahil", "Khan", "Ambazari", "Nagpur", "MHGJ", "447141", "987962080", "sahil@gmail.com"),
+    new Contacts("Saurav", "Abc", "Colony", "Banglore", "UPKJ", "447554", "887962080", "saurav@gmail.com"),
+    new Contacts("Ankit", "Xyz", "Patna", "Delhi", "WBDL", "541441", "123962080", "ankit@gmail.com"));
 
 for (let i = 0; i < arr.length; i++) {
     console.log(arr[i].toString());
@@ -158,6 +160,7 @@ function printArray() {
         console.log(arr[i].toString());
     }
 }
+//UC5: delete contacts
 deleteContacts();
 function deleteContacts() {
     let firstNamedelete = prompt4("Enter the First Name you like to delete:");
@@ -184,9 +187,21 @@ function contactChecker(firstNamedelete, check) {
 
 }
 printArray();
+//UC6: count contacts
 let count=countContacts();
 console.log("total count of contacts are: "+count)
 function countContacts(){
     return arr.map(contact => contact._firstName)
     .reduce((start,firstName)=>start+=1,0);
+}
+searchPersonthroughCityorstate();
+function searchPersonthroughCityorstate() {
+    let parameter = prompt4("Select Search Parameter 1. City 2. State :");
+    if(parameter == 1) {
+        let city = prompt4("Enter the City to Search Person :");
+        console.log(arr.filter(contact => contact._city == city).map(contact => contact));
+    } else {
+        let state = prompt4("Enter the State to Search Person :");
+        console.log(arr.filter(contact => contact._state == state).map(contact => contact));
+    }
 }
